4998fee442aaafde1a603a98938f3597f0e97425
FenghenHome <s.sunfeihu@gmail.com>
2017-09-09 12:51:10 +0800

    update more files
    Signed-off-by: FenghenHome <s.sunfeihu@gmail.com>

--- openwrt/dnsmasq.sh
+++ openwrt/dnsmasq.sh
@@ -4,7 +4,7 @@ cnlist() {
     wget -4 --no-check-certificate -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
     wget -4 --no-check-certificate -O bogus-nxdomain.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
     wget -4 --no-check-certificate -O google.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf
-    sed -i "s/114.114.114.114/115.159.96.69/g" *.conf
+    sed -i "s/114.114.114.114/115.159.220.214/g" *.conf
 }
 
 adblock() {
