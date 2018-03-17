8f564bdc744391c9fa918afc56ecd522d17afa96
fenghen <s.sunfeihu@gmail.com>
2018-03-17 13:16:14 +0800

    Update openwrt
    Signed-off-by: fenghen <s.sunfeihu@gmail.com>

--- openwrt/dnsmasq.sh
+++ openwrt/dnsmasq.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-rm -rf accelerated-domains.china.conf bogus-nxdomain.china.conf adblock.conf china_ssr.txt gfw_list.conf ignore.list
+rm -rf accelerated-domains.china.conf bogus-nxdomain.china.conf adblock-domains.china.conf ignore-ips.china.conf gfw-domains.china.conf ignore.list
 cnlist() {
     wget -4 --no-check-certificate -O accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
     wget -4 --no-check-certificate -O bogus-nxdomain.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/bogus-nxdomain.china.conf
     wget -4 --no-check-certificate -O bogus-nxdomain.china.ext.conf https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf
     wget -4 --no-check-certificate -O google.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf
     wget -4 --no-check-certificate -O apple.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf
-    wget -4 --no-check-certificate -O china_ssr.txt https://raw.githubusercontent.com/LisonFan/china_ip_list/master/china_ip_list
+    wget -4 --no-check-certificate -O ignore-ips.china.conf https://raw.githubusercontent.com/LisonFan/china_ip_list/master/china_ip_list
 
     # DNS:https://puredns.cn、https://pdomo.me、http://aixyz.com、http://www.fundns.cn、https://www.onedns.net、https://baidns.cn
     sed -i "s/114.114.114.114/123.207.137.88/g" *.conf
@@ -32,27 +32,27 @@ cnlist() {
 adblock() {
     wget -4 --no-check-certificate -O - https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt |
     grep ^\|\|[^\*]*\^$ |
-    sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | uniq > adblock.conf
+    sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | uniq > adblock-domains.china.conf
 
     wget -4 --no-check-certificate -O - https://raw.githubusercontent.com/kcschan/AdditionalAdblock/master/list.txt |
     grep ^\|\|[^\*]*\^$ |
     sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' | uniq > adblock.ext.conf
 
-    # adblock.conf
-    cat adblock.conf adblock.ext.conf > file.txt
-    rm -rf adblock.conf adblock.ext.conf
-    mv file.txt adblock.conf
+    # adblock-domains.china.conf
+    cat adblock-domains.china.conf adblock.ext.conf > file.txt
+    rm -rf adblock-domains.china.conf adblock.ext.conf
+    mv file.txt adblock-domains.china.conf
     bash blockad.sh
-    cat adblock.conf blockad.conf > file.txt
-    rm -rf adblock.conf blockad.conf
-    mv file.txt adblock.conf
+    cat adblock-domains.china.conf blockad.conf > file.txt
+    rm -rf adblock-domains.china.conf blockad.conf
+    mv file.txt adblock-domains.china.conf
     bash mwsl.sh
-    cat adblock.conf mal-hostlist.conf > file.txt
-    rm -rf adblock.conf mal-hostlist.conf
-    mv file.txt adblock.conf
-    sort -n adblock.conf | uniq
-    sort -n adblock.conf | awk '{if($0!=line)print; line=$0}'
-    sort -n adblock.conf | sed '$!N; /^\(.*\)\n\1$/!P; D'
+    cat adblock-domains.china.conf mal-hostlist.conf > file.txt
+    rm -rf adblock-domains.china.conf mal-hostlist.conf
+    mv file.txt adblock-domains.china.conf
+    sort -n adblock-domains.china.conf | uniq
+    sort -n adblock-domains.china.conf | awk '{if($0!=line)print; line=$0}'
+    sort -n adblock-domains.china.conf | sed '$!N; /^\(.*\)\n\1$/!P; D'
 }
 
 ignore() {
@@ -60,7 +60,7 @@ ignore() {
 }
 
 gfwlist() {
-    wget https://raw.githubusercontent.com/cokebar/gfwlist2dnsmasq/master/gfwlist2dnsmasq.sh && chmod +x gfwlist2dnsmasq.sh && bash gfwlist2dnsmasq.sh -s gfwlist -o gfw_list.conf
+    wget https://raw.githubusercontent.com/cokebar/gfwlist2dnsmasq/master/gfwlist2dnsmasq.sh && chmod +x gfwlist2dnsmasq.sh && bash gfwlist2dnsmasq.sh -s gfwlist -o gfw-domains.china.conf
     rm -rf gfwlist2dnsmasq.sh
 }
 
