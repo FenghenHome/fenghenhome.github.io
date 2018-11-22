97699d0688912bc227e2930a24cbfbef05b05db5
FenghenHome <FenghenHome@users.noreply.github.com>
2018-11-22 21:36:40 +0800

    Update dnsmasq.sh

--- openwrt/dnsmasq.sh
+++ openwrt/dnsmasq.sh
@@ -9,7 +9,7 @@ cnlist() {
     # china_ipv4_ipv6_list：https://raw.githubusercontent.com/LisonFan/china_ip_list/master/china_ipv4_ipv6_list
     wget -4 -O ignore-ips.china.conf https://raw.githubusercontent.com/LisonFan/china_ip_list/master/china_ipv4_list
 
-    # DNS:https://puredns.cn、https://pdomo.me、http://aixyz.com、http://www.fundns.cn、https://www.onedns.net、https://baidns.cn
+    # DNS:https://puredns.cn、https://pdomo.me、https://www.onedns.net、https://hixns.cn
     sed -i "s/114.114.114.114/123.207.13.111/g" *.conf
 
     # bogus-nxdomain.china.conf
