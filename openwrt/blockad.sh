1813f73029dcd02c0e362ce63577a88893586133
FenghenHome <FenghenHome@users.noreply.github.com>
2018-11-22 21:47:11 +0800

    Update blockad.sh

--- openwrt/blockad.sh
+++ openwrt/blockad.sh
@@ -14,6 +14,7 @@ for URL in \
     "https://raw.githubusercontent.com/vokins/yhosts/master/hosts" \
     "https://raw.githubusercontent.com/neoFelhz/neohosts/gh-pages/full/hosts" \
     "https://www.malwaredomainlist.com/hostslist/hosts.txt" \
+    "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts" \
     "https://adaway.org/hosts.txt"
 do
     # filter out comment lines, empty lines, localhost... 
