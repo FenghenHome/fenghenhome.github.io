65f29eda1da7888b85694f8e7c629dd4ffc12e5b
FenghenHome <s.sunfeihu@gmail.com>
2019-01-27 23:54:47 +0800

    Update *.conf

--- openwrt/dnsmasq.sh
+++ openwrt/dnsmasq.sh
@@ -9,7 +9,7 @@ cnlist() {
     wget -4 -O ignore-ips.china.conf https://raw.githubusercontent.com/LisonFan/china_ip_list/master/china_ipv4_list
 
     # DNS:https://puredns.cn、https://pdomo.me、https://www.onedns.net、https://hixns.cn
-    sed -i "s/114.114.114.114/193.112.15.186/g" *.conf
+    sed -i "s/114.114.114.114/119.29.29.29/g" *.conf
 
     # bogus-nxdomain.china.conf
     cat bogus-nxdomain.china.conf bogus-nxdomain.china.ext.conf > file.txt
